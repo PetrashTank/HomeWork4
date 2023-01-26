4f8247763770a60b8827694670b19354df0738a3
Tatiana <zanka_16@mail.ru>
2023-01-26 14:20:10 +0800

    Задание 2 ДЗ4

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int amount = number.ToString().Length;
+string str = number.ToString();
+int summ = 0;
+int[] b = new int[amount];
+
+for( int i=0; i< amount; i++)
+{
+    b[i] = int.Parse(str[i].ToString());
+}
+for (int i = 0; i < b.Length; i++)
+{
+    summ = b[i] + summ;
+}
+
+Console.WriteLine(summ);
