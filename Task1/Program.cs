b717d79dd4b60eccd4e146087c72f5e3fe31b1d1
Tatiana <zanka_16@mail.ru>
2023-01-26 14:10:31 +0800

    Задание 1 ДЗ4

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+double GetDegree()
+{
+    Console.WriteLine("Введите степень");
+    int gegree = Convert.ToInt32(Console.ReadLine());
+    return  gegree;
+}
+int N = GetNumber();
+double D = GetDegree();
+
+    Console.Write($"{N}^{D} = {(long)Math.Pow(N, D)}");
+   
